de949e5d8f0e9ca145ce7af75813dbd1f379613f
Jonas sousa <jonassousagtr@gmail.com>
2023-04-03 06:05:41 -0300

    Feat: Criando estrutura base do cabeçalho.

---
+++ src/componentes/Cabecalho/index.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.png'
+import search from './search.png'
+
+export default function Cabecalho () {
+    return (
+        <header>
+            <img src={logo} alt="Logo do Alura space" />
+            <div>
+                <input 
+                    type="text"
+                    placeholder="O que você procura?" 
+                />
+                <img src={search} alt="Ícone de lupa" />
+            </div>
+        </header>
+    )
+}
