d29c6c96897079eec87138a0fab212115e050a3e
Jonas sousa <jonassousagtr@gmail.com>
2023-04-05 06:37:11 -0300

    Feat: Adicionando o banner no site.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-<<<<<<<<< Temporary merge branch 1
 import PaginaInicial from "./paginas/PaginaInicial";
-=========
 import styles from './styles/estilosGlobais.scss'
->>>>>>>>> Temporary merge branch 2
+
 
 export default function App() {
   return (
