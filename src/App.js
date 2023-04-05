469a3582dbfbd863e35b7471899bcdc5d7be48bc
Jonas sousa <jonassousagtr@gmail.com>
2023-04-05 06:16:58 -0300

    Feat: Construindo o menu e estilizando o componente.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import PaginaInicial from "./paginas/PaginaInicial";
+import styles from "./styles/estilosGlobais.scss"
 
 export default function App() {
   return (
+   
     <PaginaInicial/>
+     
   );
 }
